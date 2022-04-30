fa7602bfd043dad5d14f83a2ae1cc88345c3582a
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-04-30 04:59:29 +0300

    Serilog Json Format

--- Serilog.Console/Program.cs
+++ Serilog.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog.Formatting.Compact;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,9 @@ namespace Serilog.Console
             Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Information()
             .WriteTo.Console()
+            //.WriteTo.File(new CompactJsonFormatter(),"log.json",
+            //    rollingInterval: RollingInterval.Day,
+            //    rollOnFileSizeLimit: true)
             .WriteTo.File("log.txt",
                 rollingInterval: RollingInterval.Day,
                 rollOnFileSizeLimit: true)
