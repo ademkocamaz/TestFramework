8b07b3d3e6d62e0a33ce2d24b3d2547358a3fcb9
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-04-30 04:49:38 +0300

    Added Project Serilog.Console

---
+++ Serilog.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File("log.txt",
+                rollingInterval: RollingInterval.Day,
+                rollOnFileSizeLimit: true)
+            .CreateLogger();
+
+            Log.Information("Hello, Serilog!");
+
+
+            try
+            {
+                throw new Exception();
+            }
+            catch (Exception exception)
+            {
+
+                Log.Error(exception, "Hata oluştu.");
+            }
+
+            Log.CloseAndFlush();
+            System.Console.ReadKey();
+        }
+    }
+}
